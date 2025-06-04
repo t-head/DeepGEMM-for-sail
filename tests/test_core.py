b2bc61e1bfe3967e4ca7224a83e73fcbdb223fb9
taotao.wt <taotao.wt@alibaba-inc.com>
2025-06-04 16:36:46 +0800

    align deepgemm test_core.py to latest opensource for better perf comparison
    Change-Id: I2a96f4a98894c5b39ebadf6611f145e6fa0fd80b

--- tests/test_core.py
+++ tests/test_core.py
@@ -41,7 +41,6 @@ def test_gemm(d: torch.dtype) -> None:
     print('Testing GEMM:')
     for m in (64, 128, 4096):
         for k, n in [(576, 7168), (7168, 2112), (1536, 24576), (512, 32768), (16384, 7168), (7168, 4096), (2048, 7168)]:
-    
     # m = 32
     # n = 32
     # k = 256
@@ -73,32 +72,26 @@ def test_gemm(d: torch.dtype) -> None:
 
 def construct_contiguous_grouped(num_groups: int, expected_m_per_group: int, k: int, n: int, d: torch.dtype) -> \
         Tuple[int, Tuple[torch.Tensor, torch.Tensor], Tuple[torch.Tensor, torch.Tensor], torch.Tensor, torch.Tensor, torch.Tensor]:
-    m_aligned = get_m_alignment_for_contiguous_layout()
-    group_m_list = []
-
-    m = 0
-    for i in range(num_groups):
-        group_m = m_aligned * random.randint(int(expected_m_per_group * 0.7) // m_aligned, int(expected_m_per_group * 1.3) // m_aligned)
-        m += group_m
-        group_m_list.append(group_m)
+    alignment = get_m_alignment_for_contiguous_layout()
+    group_ms = [int(expected_m_per_group * random.uniform(0.7, 1.3)) for _ in range(num_groups)]
+    m = sum([ceil_div(x, alignment) * alignment for x in group_ms])
 
     x = torch.randn((m, k), device='cuda', dtype=torch.bfloat16)
-    y = torch.randn((num_groups, n, k), device='cuda', dtype=torch.bfloat16)
-
-    # x = torch.ones((m, k), device='cuda', dtype=torch.bfloat16)
-    # y = torch.ones((num_groups, n, k), device='cuda', dtype=torch.bfloat16)
-
+    y = torch.randn((num_groups, n, k), device='cuda', dtype=torch.bfloat16)    
     m_indices = torch.empty(m, device='cuda', dtype=torch.int32)
     out = torch.empty((m, n), device='cuda', dtype=torch.bfloat16)
     ref_out = torch.randn((m, n), device='cuda', dtype=torch.bfloat16)
-
+    
     start = 0
     
-    for i, group_m in enumerate(group_m_list):
-        end = start + group_m
-        m_indices[start:end] = i
-        ref_out[start:end] = x[start:end] @ y[i].t()
-        start = end
+    for i, group_m in enumerate(group_ms):
+        actual_end = start + group_m
+        aligned_end = start + ceil_div(group_m, alignment) * alignment
+        m_indices[start:actual_end] = i
+        m_indices[actual_end:aligned_end] = -1
+        ref_out[start:aligned_end] = x[start:aligned_end] @ y[i].t()
+        start = aligned_end
+    ref_out = torch.where((m_indices == -1).unsqueeze(1), torch.zeros_like(ref_out), ref_out)
     
     if d == torch.bfloat16:
         return m, x, y, m_indices, out, ref_out
@@ -113,87 +106,76 @@ def construct_contiguous_grouped(num_groups: int, expected_m_per_group: int, k: 
 def test_m_grouped_gemm_contiguous(d: torch.dtype) -> None:
     print('Testing grouped contiguous GEMM:')
 
-    for num_groups, expected_m_per_group, k, n in ((4, 8192, 7168, 4096), (4, 8192, 2048, 7168), (8, 4096, 7168, 4096), (8, 4096, 2048, 7168)):
-        # TODO: make a stronger test
-            # num_groups = 16
-            # expected_m_per_group = 128
-            # k = 16
-            # n = 32
+    for num_groups, expected_m_per_group, k, n in ((4, 8192, 7168, 4096), (4, 8192, 2048, 7168),
+                                                   (8, 4096, 7168, 4096), (8, 4096, 2048, 7168),
+                                                   (32, 256, 7168, 4096), (32, 256, 2048, 7168)):
+        # NOTES: we should mask the unfilled part before calculating difference
+        m, x, y, m_indices, out, ref_out = construct_contiguous_grouped(num_groups, expected_m_per_group, k, n, d)
+        if (d == torch.bfloat16):
+            deep_gemm.m_grouped_gemm_bf16_bf16_bf16_nt_contiguous(x, y, out, m_indices)
+        else:
+            deep_gemm.m_grouped_gemm_int8_int8_bf16_nt_contiguous(x, y, out, m_indices)
+        out = torch.where((m_indices == -1).unsqueeze(1), torch.zeros_like(out), out)
+        diff = calc_diff(out, ref_out)
+        assert diff < 0.001, f'{m=}, {k=}, {n=}, {diff:.5f}'
 
-            m, x, y, m_indices, out, ref_out = construct_contiguous_grouped(num_groups, expected_m_per_group, k, n, d)
+        # Construct new tensors only once to avoid L2 cache acceleration (creating them puts them in L2)
+        m, x, y, m_indices, out, ref_out = construct_contiguous_grouped(num_groups, expected_m_per_group, k, n, d)
+
+        # noinspection PyShadowingNames
+        def test_func():
             if (d == torch.bfloat16):
                 deep_gemm.m_grouped_gemm_bf16_bf16_bf16_nt_contiguous(x, y, out, m_indices)
             else:
                 deep_gemm.m_grouped_gemm_int8_int8_bf16_nt_contiguous(x, y, out, m_indices)
-            diff = calc_diff(out, ref_out)
 
-            assert diff < 0.001, f'{m=}, {k=}, {n=}, {diff:.5f}'
-
-            # Construct new tensors only once to avoid L2 cache acceleration (creating them puts them in L2)
-            m, x, y, m_indices, out, ref_out = construct_contiguous_grouped(num_groups, expected_m_per_group, k, n, d)
-
-            # noinspection PyShadowingNames
-            def test_func():
-                if (d == torch.bfloat16):
-                    deep_gemm.m_grouped_gemm_bf16_bf16_bf16_nt_contiguous(x, y, out, m_indices)
-                else:
-                    deep_gemm.m_grouped_gemm_int8_int8_bf16_nt_contiguous(x, y, out, m_indices)
-
-            t = bench_kineto(test_func, 'gemm', suppress_kineto_output=True)
-            print(f' > Perf ((contiguous dtype={str(d)}, {num_groups=:2}, {expected_m_per_group=:4}, n={n:4}, k={k:4}): {t * 1e6:4.0f} us | '
-              f'throughput: {2 * m * n * k / t / 1e12:4.0f} TFLOPS, '
-              f'{(m * k + num_groups * k * n + m * n * 2) / 1e9 / t:4.0f} GB/s')
+        t = bench_kineto(test_func, 'gemm', suppress_kineto_output=True)
+        print(f' > Perf ((contiguous dtype={str(d)}, {num_groups=:2}, {expected_m_per_group=:4}, n={n:4}, k={k:4}): {t * 1e6:4.0f} us | '
+            f'throughput: {2 * m * n * k / t / 1e12:4.0f} TFLOPS, '
+            f'{(m * k + num_groups * k * n + m * n * 2) / 1e9 / t:4.0f} GB/s')
     print("Passed\n")
 
 
-def construct_grouped_masked(num_groups: int, m: int, k: int, n: int, d: torch.dtype):
-    x = torch.randn((num_groups, m, k), device='cuda', dtype=torch.bfloat16)
+def construct_grouped_masked(num_groups: int, max_m: int, expected_m_per_group: int, k: int, n: int, d: torch.dtype):
+    x = torch.randn((num_groups, max_m, k), device='cuda', dtype=torch.bfloat16)
     y = torch.randn((num_groups, n, k), device='cuda', dtype=torch.bfloat16)
 
     # x = torch.ones((num_groups, m, k), device='cuda', dtype=torch.bfloat16)
     # y = torch.ones((num_groups, n, k), device='cuda', dtype=torch.bfloat16)
 
-    out = torch.empty((num_groups, m, n), device='cuda', dtype=torch.bfloat16)
+    out = torch.empty((num_groups, max_m, n), device='cuda', dtype=torch.bfloat16)
     ref_out = torch.einsum('gmk,gnk->gmn', x, y)
 
+    # Construct mask
+    masked_m = torch.empty((num_groups, ), device='cuda', dtype=torch.int)
+    for j in range(num_groups):
+        masked_m[j] = int(expected_m_per_group * random.uniform(0.7, 1.3))
+    assert masked_m.amax().item() <= max_m
+
     if d == torch.bfloat16:
-        return x, y, out, ref_out
+        return x, y, masked_m, out, ref_out
     else:
-        x_int8 = (torch.empty_like(x, dtype=torch.int8), torch.empty((num_groups, m, 1), device='cuda', dtype=torch.float))
+        x_int8 = (torch.empty_like(x, dtype=torch.int8), torch.empty((num_groups, max_m, 1), device='cuda', dtype=torch.float))
         y_int8 = (torch.empty_like(y, dtype=torch.int8), torch.empty((num_groups, n, 1), device='cuda', dtype=torch.float))
         for i in range(num_groups):
             x_int8[0][i], x_int8[1][i] = per_token_cast_to_int8(x[i])
             y_int8[0][i], y_int8[1][i] = per_token_cast_to_int8(y[i])
 
-        return x_int8, y_int8, out, ref_out
+        return x_int8, y_int8, masked_m, out, ref_out
 
 def test_m_grouped_gemm_masked(d: torch.dtype) -> None:
     print('Testing grouped masked GEMM:')
 
-    for num_groups, m in ((1, 1024), (2, 512), (4, 256), (32, 128), (64, 64), (256, 16)):
+    for num_groups, expected_m_per_group in ((1, 1024), (2, 512), (4, 256)):
         for k, n in ((7168, 4096), (2048, 7168), ):
         # Test correctness
-
-    # num_groups = 2
-    # m = 32
-    # k = 128
-    # n = 32
-
-            masked_m_candidates = list(filter(lambda candidate: candidate <= m, (8, 10, 20, 35, 50, 64, 128, 192, 256, 320, 384)))
             for i in range(10):
-                x, y, out, ref_out = construct_grouped_masked(num_groups, m, k, n, d)
-
-                masked_m = torch.empty((num_groups, ), device='cuda', dtype=torch.int)
-
-                for j in range(num_groups):
-                    masked_m[j] = random.choice(masked_m_candidates)
-                expected_m = min(int(masked_m.float().mean()) + 1, m)
+                x, y, masked_m, out, ref_out = construct_grouped_masked(num_groups, 4096, expected_m_per_group, k, n, d)
 
                 if (d == torch.bfloat16):
-                    deep_gemm.m_grouped_gemm_bf16_bf16_bf16_nt_masked(x, y, out, masked_m, expected_m)
+                    deep_gemm.m_grouped_gemm_bf16_bf16_bf16_nt_masked(x, y, out, masked_m, expected_m_per_group)
                 else:
-                    deep_gemm.m_grouped_gemm_int8_int8_bf16_nt_masked(x, y, out, masked_m, expected_m)
-
+                    deep_gemm.m_grouped_gemm_int8_int8_bf16_nt_masked(x, y, out, masked_m, expected_m_per_group)
 
                 for j in range(num_groups):
                     diff = calc_diff(out[j, :masked_m[j].item()], ref_out[j, :masked_m[j].item()])
@@ -202,9 +184,9 @@ def test_m_grouped_gemm_masked(d: torch.dtype) -> None:
                 # noinspection PyShadowingNames
                 def test_func():
                     if (d == torch.bfloat16):
-                        deep_gemm.m_grouped_gemm_bf16_bf16_bf16_nt_masked(x, y, out, masked_m, expected_m)
+                        deep_gemm.m_grouped_gemm_bf16_bf16_bf16_nt_masked(x, y, out, masked_m, expected_m_per_group)
                     else:
-                        deep_gemm.m_grouped_gemm_int8_int8_bf16_nt_masked(x, y, out, masked_m, expected_m)
+                        deep_gemm.m_grouped_gemm_int8_int8_bf16_nt_masked(x, y, out, masked_m, expected_m_per_group)
 
                     # Test performance with fixed shapes
                     # noinspection PyUnboundLocalVariable
