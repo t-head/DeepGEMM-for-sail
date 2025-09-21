7e6c3a3d38575032f841bd07f6f3c0efd67cf53c
mengwu.zxh <mengwu.zxh@alibaba-inc.com>
2025-09-21 10:42:28 +0800

    deepgemm: enable warp0 aiu to support blockM=48,192
    to #70342365: ppu1.5 moe a8w8 support
    Test Case: test_core.py
    Reviewed By: taotao, zhenghai
    
    Change-Id: I8c48f20074b935ee6a8e97f9c5832aed858338ce

--- deep_gemm/include/deep_gemm/int8_gemm_cutlass3.cuh
+++ deep_gemm/include/deep_gemm/int8_gemm_cutlass3.cuh
@@ -53,6 +53,138 @@ inline int compute_occupancy_for_kernel()
   return max_active_blocks;
 }
 
+enum class AiuSplitConfig {
+  AiuSplitBaseline,
+  AiuOnWarp0A1B,
+  AiuOnWarp0,
+};
+
+template <
+  int BlockM,
+  int BlockN
+>
+CUTE_HOST_DEVICE constexpr AiuSplitConfig get_aiu_split_config() {
+  return (BlockM % 48 == 0 || (BlockM == 256 && BlockN == 256)) ? AiuSplitConfig::AiuOnWarp0 : AiuSplitConfig::AiuSplitBaseline;
+}
+
+
+using namespace cute;
+template <
+  AiuSplitConfig SplitConfig,
+  class CopyPolicyA,
+  class SrcEngineA, class SrcLayoutA,
+  class DstEngineA, class DstLayoutA,
+  class CopyPolicyB,
+  class SrcEngineB, class SrcLayoutB,
+  class DstEngineB, class DstLayoutB
+>
+CUTE_HOST_DEVICE
+void
+copy_aiu_v2(
+  CopyPolicyA                  const& copy_policy_a,
+  Tensor<SrcEngineA, SrcLayoutA> const& src_a,
+  Tensor<DstEngineA, DstLayoutA>     && dst_a,
+  CopyPolicyB                  const& copy_policy_b,
+  Tensor<SrcEngineB, SrcLayoutB> const& src_b,
+  Tensor<DstEngineB, DstLayoutB>     && dst_b,
+  int &warp_idx
+) {
+  if constexpr (SplitConfig == AiuSplitConfig::AiuOnWarp0A1B) {
+    if (warp_idx == 0) {
+      copy(copy_policy_a, src_a, dst_a);
+    } else if (warp_idx == 1) {
+      copy(copy_policy_b, src_b, dst_b);
+    }
+  } else if constexpr (SplitConfig == AiuSplitConfig::AiuOnWarp0) {
+    if (warp_idx == 0) {
+      copy(copy_policy_a, src_a, dst_a);
+      copy(copy_policy_b, src_b, dst_b);
+    }
+  } else if constexpr (SplitConfig == AiuSplitConfig::AiuSplitBaseline) {
+    if (warp_idx == 0) {
+      CUTLASS_PRAGMA_UNROLL
+      for (int i = 0; i < size<1>(src_a) / 2; ++i) {
+        copy(copy_policy_a, src_a(_,i,_), dst_a(_,i,_));
+      }
+      CUTLASS_PRAGMA_UNROLL
+      for (int i = 0; i < size<1>(src_b) / 2; ++i) {
+        copy(copy_policy_b, src_b(_,i,_), dst_b(_,i,_));
+      }
+    }
+    else if (warp_idx == 1) {
+      CUTLASS_PRAGMA_UNROLL
+      for (int i = size<1>(src_a) / 2; i < size<1>(src_a); ++i) {
+        copy(copy_policy_a, src_a(_,i,_), dst_a(_,i,_));
+      }
+      CUTLASS_PRAGMA_UNROLL
+      for (int i = size<1>(src_b) / 2; i < size<1>(src_b); ++i) {
+        copy(copy_policy_b, src_b(_,i,_), dst_b(_,i,_));
+      }
+    }
+  }
+
+}
+
+template <
+  AiuSplitConfig SplitConfig,
+  typename Element,
+  bool Trans,
+  typename Block_MN,
+  typename Block_K,
+  bool Swap,
+  int StageStride = 1
+> struct DefaultGemm_AIU_Operand_v2;
+
+template <
+  AiuSplitConfig SplitConfig,
+  typename Element,
+  typename Block_MN,
+  typename Block_K,
+  bool Swap,
+  int StageStride
+> struct DefaultGemm_AIU_Operand_v2<
+  SplitConfig,
+  Element,
+  false,
+  Block_MN,
+  Block_K,
+  Swap,
+  StageStride
+> {
+
+  static_assert(Block_K{} * sizeof_bits<Element>::value * 8 % 32 == 0, "aiu_no_trans: block_k must be multiple of 32B");
+  static constexpr int BlockContSize = Block_K{} * sizeof_bits<Element>::value / 8;
+  static_assert(BlockContSize > 128 ? (BlockContSize % 128 == 0) : (BlockContSize % 32 == 0), "aiu_trans: block contiguous size should be multiple of 128B or 32B");
+  static constexpr int AiuContByteSize = BlockContSize > 128 ? 128 : BlockContSize;
+  static constexpr int AiuContElemSize = AiuContByteSize / sizeof_bits<Element>::value * 8;
+  static constexpr bool split_on_k = AiuContElemSize < Block_K{};
+  // each aiu_load should at least cover one mma
+  static constexpr bool split_on_mn = SplitConfig == AiuSplitConfig::AiuSplitBaseline;
+  static constexpr int inst_num = split_on_k ? (Block_K{} / AiuContElemSize) : (split_on_mn ? 2 : 1);
+
+  // will only split on one dimension
+  static constexpr int CUBE_H = split_on_k ? Block_MN{} : (Block_MN{} / inst_num);
+  static constexpr int CUBE_W = AiuContElemSize;
+  static constexpr int bits_per_aiu = CUBE_H * CUBE_W * sizeof_bits<Element>::value;
+  using CopyInst = Acompute10500_AIU_LOAD<cute::C<bits_per_aiu>, Element, false, CUBE_H, CUBE_W>;
+  using SmemCopyOp = Acompute10500_TSM_LD_SWZL<Element, CUBE_H, CUBE_W, Swap, false, inst_num * StageStride>;
+
+  using GmemTiledCopy = decltype(
+    make_tiled_copy(Copy_Atom<CopyInst, Element>{},
+                    Layout<Shape <_1,_1>,
+                           Stride<_1,_1>>{},
+                    Layout<Shape <Int<CUBE_H>, Int<CUBE_W>>>{}));
+
+  using SmemCopyAtom = Copy_Atom<SmemCopyOp, Element>;
+  // to make smem_layout[0] or smem_layout[1] to be two dimensional, SmemLayoutAtom.stride is not used in aiu gemm
+  using SmemAtomStride = typename cutlass::platform::conditional<
+    split_on_k,
+    Stride<Int<CUBE_W>, _1>,
+    Stride<_1, Int<CUBE_H>>
+  >::type;
+  using SmemLayoutAtom = Layout<Shape<Int<CUBE_H>, Int<CUBE_W>>, SmemAtomStride>;
+};
+
 using namespace cute;
 
 namespace cutlass::gemm {
@@ -768,6 +900,8 @@ struct CollectiveMma<
   static constexpr int ScaleMsPerThread = cute::ceil_div(size<0>(TileShape{}), Int<MaxThreadsPerBlock * ScaleGranularity>{});
   static constexpr int ScaleNsPerThread = cute::ceil_div(size<1>(TileShape{}), Int<MaxThreadsPerBlock * ScaleGranularity>{});
 
+  static constexpr auto SplitAIU = get_aiu_split_config<CTA_M, CTA_N>();
+
   // ScaleA
   using GmemTiledCopyScaleA = decltype(
     make_tiled_copy(Copy_Atom<SM80_CP_ASYNC_CACHEGLOBAL<ScaleCopyAtomWidth>, ElementScale>{},
@@ -998,7 +1132,7 @@ struct CollectiveMma<
     // Start async loads for all pipes but the last
     CUTLASS_PRAGMA_UNROLL
     for (int k_pipe = 0; k_pipe < DispatchPolicy::Stages; ++k_pipe) {
-      copy_aiu(
+      copy_aiu_v2<SplitAIU>(
         gmem_tiled_copy_A, tAgA(_,_,_,*k_tile_iter), tAsA(_,_,_,k_pipe),
         gmem_tiled_copy_B, tBgB(_,_,_,*k_tile_iter), tBsB(_,_,_,k_pipe),
         warp_idx
@@ -1129,7 +1263,7 @@ CUTLASS_PRAGMA_UNROLL
           __syncthreads();
 
           if (k_tile_count > 0) {
-            copy_aiu(
+            copy_aiu_v2<SplitAIU>(
               gmem_tiled_copy_A, tAgA(_,_,_,*k_tile_iter), tAsA(_,_,_,smem_pipe_write),
               gmem_tiled_copy_B, tBgB(_,_,_,*k_tile_iter), tBsB(_,_,_,smem_pipe_write),
               warp_idx
@@ -1544,8 +1678,11 @@ public:
         static constexpr bool TransA = cutlass::platform::is_same<LayoutA, cutlass::layout::RowMajor>::value ? false : true;
         static constexpr bool TransB = cutlass::platform::is_same<LayoutB, cutlass::layout::ColumnMajor>::value ? false : true;
         static constexpr int TSM_LD_NUM = BLOCK_M == 8 ? 2 : 4;
-        using DefaultOperandA = cutlass::gemm::config::DefaultGemm_AIU_Operand<ElementA, TransA, Int<BLOCK_M>, Int<BLOCK_K>, false>;
-        using DefaultOperandB = cutlass::gemm::config::DefaultGemm_AIU_Operand<ElementB, TransB, Int<BLOCK_N>, Int<BLOCK_K>, true>;
+
+        static constexpr auto SplitAIU = get_aiu_split_config<BLOCK_M, BLOCK_N>();
+
+        using DefaultOperandA = DefaultGemm_AIU_Operand_v2<SplitAIU, ElementA, TransA, Int<BLOCK_M>, Int<BLOCK_K>, false>;
+        using DefaultOperandB = DefaultGemm_AIU_Operand_v2<SplitAIU, ElementB, TransB, Int<BLOCK_N>, Int<BLOCK_K>, true>;
         // A
         using SmemLayoutAtomA = typename DefaultOperandA::SmemLayoutAtom; // M, K
         using SmemCopyAtomA = typename DefaultOperandA::SmemCopyAtom;
