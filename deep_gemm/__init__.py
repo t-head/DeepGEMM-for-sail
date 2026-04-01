68a716ce40cf511f87d652d5cb82448054cef0ad
ywj496888 <ywj496888@alibaba-inc.com>
2026-04-01 16:33:10 +0800

    [fp8 gemm]: ali gemm_fp8_fp8_bf16_nt API with community
    to #79673810: att
    Test Case: fp8 gemm
    Reviewed By: Yao tiesheng, Shen Zhenghai
    
    Change-Id: I63a8cb30ed7574b6b9f2238e8ea6bc7c167c06be

--- deep_gemm/__init__.py
+++ deep_gemm/__init__.py
@@ -40,11 +40,6 @@ from .jit_kernels import (
     int8_paged_mqa_logits,
 )
 
-# Some alias for APIs
-fp8_m_grouped_gemm_nt_masked = m_grouped_gemm_fp8_fp8_bf16_nt_masked
-m_grouped_fp8_gemm_nt_contiguous = m_grouped_gemm_fp8_fp8_bf16_nt_contiguous
-get_mn_major_tma_aligned_tensor = get_col_major_tma_aligned_tensor
-
 from .utils import (
     bench,
     bench_kineto,
@@ -82,3 +77,9 @@ if should_init_deep_gemm_cpp:
     )
 else:
     from .jit_kernels import gemm_fp8_fp8_bf16_nt
+
+# Some alias for APIs
+fp8_gemm_nt = gemm_fp8_fp8_bf16_nt
+fp8_m_grouped_gemm_nt_masked = m_grouped_gemm_fp8_fp8_bf16_nt_masked
+m_grouped_fp8_gemm_nt_contiguous = m_grouped_gemm_fp8_fp8_bf16_nt_contiguous
+get_mn_major_tma_aligned_tensor = get_col_major_tma_aligned_tensor
